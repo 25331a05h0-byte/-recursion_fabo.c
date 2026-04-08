e1319abf651ca8b6dc9ab77575417052e98c3cd2
25331a05h0-byte <25331a05h0@mvgrce.edu.in>
2026-04-08 06:10:39 +0530

    Update recursive_fibinocci.c

--- recursive_fibinocci.c
+++ recursive_fibinocci.c
@@ -1,26 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int fib(int n)
-{
-    if(n == 0 || n == 1)
-        return n;
-    else
-        return fib(n-1) + fib(n-2);
-}
+// Function declaration
+int fibonacci(int n);
 
-int main()
-{
-    int i, n;
+// Main function
+int main() {
+    int n, i;
 
     printf("Enter number of terms: ");
     scanf("%d", &n);
 
     printf("Fibonacci Series: ");
-
-    for(i = 0; i < n; i++)
-    {
-        printf("%d ", fib(i));
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));   // Calling recursive function
     }
-
+    printf("\n");
+printf(“\n               25331A05H0”);
     return 0;
+}
+
+// Recursive function to get nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
